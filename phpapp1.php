a74d717402f6c4c17364c838e213c6bb4ec53a74
thankyou123456789 <69336071+thankyou123456789@users.noreply.github.com>
2021-12-21 18:43:26 +0700

    Add files via upload

---
+++ phpapp1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE htmlw>
+<html>
+
+<head>
+     <meta chaeset="utf8">
+     <meta name="viewport" content="device=width-device, initial-scale=1.0">
+     <title>PHP: Basic</title>
+     <style>
+         h1{
+             color:red;
+         }
+     </style> 
+</head>
+
+<body>
+    <h1 style="color:blueviolet";">
+        <?php echo "PHP Syntax"; ?>
+    </h1>
+</body>
+
+</html>
